544c903662f7179f7e3f9036ae164f616befdb46
Crowdbotics <engineering@crowdbotics.com>
2023-07-08 23:31:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-flower-dev-99093",
+  url: "https://broken-flower-dev-99093.botics.co",
+  api: "https://broken-flower-dev-99093.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
